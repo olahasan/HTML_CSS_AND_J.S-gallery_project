59552050f6bf2cd7f47463cf84e44824c97d5b98
olahasan <ola.ali1000@gmail.com>
2022-08-24 00:23:42 -0700

    Upload The Design

---
+++ main.js
@@ -0,0 +1,40 @@
+let img1 = document.getElementById("one");
+let img2 = document.getElementById("two");
+let img3 = document.getElementById("three");
+let img4 = document.getElementById("four");
+
+let main = document.getElementById("main");
+
+let img5 = document.getElementById("five");
+let img6 = document.getElementById("six");
+let img7 = document.getElementById("seven");
+let img8 = document.getElementById("eight");
+
+
+img1.onclick = function () {
+    main.src = this.src
+}
+img2.onclick = function () {
+    main.src = this.src
+}
+img3.onclick = function () {
+    main.src = this.src
+}
+img4.onclick = function () {
+    main.src = this.src
+}
+
+img5.onmouseenter = function () {
+    main.src = this.src
+}
+img6.onmouseenter = function () {
+    main.src = this.src
+}
+img7.onmouseenter = function () {
+    main.src = this.src
+}
+img8.onmouseenter = function () {
+    main.src = this.src
+}
+
+
